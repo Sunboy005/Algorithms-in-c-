60ae3e979b2ecf0d1b0a8855e95ddba1536514e6
Amos Sunmbo Alagbe <sunboy005@gmail.com>
2021-10-23 15:37:43 +0100

    First Algorithms

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] newArr = new int[] { 2, 3, 4, 5 };
+            foreach(int num in newArr)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
