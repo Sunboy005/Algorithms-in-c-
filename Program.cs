ac27d2c2ceeb8295152b4e5e90966e22d1f3318c
Amos Sunmbo Alagbe <sunboy005@gmail.com>
2021-10-23 22:15:27 +0100

    Working on reverse array

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace CSharpAlgorithms
 {
@@ -27,16 +29,59 @@ namespace CSharpAlgorithms
 
             Console.WriteLine("##################");//Spacing Each Solution
 
-            Console.WriteLine("Using Linq ");
+            Console.WriteLine("Using Linq given: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ]");
 
-            int[] numberArr = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-            Console.WriteLine($"The maximum number using Num.max() is {numberArr.Max()}");//
-            Console.WriteLine($"The maximum number using Num.max() is {numberArr.Min()}");//
-            Console.WriteLine($"The maximum number using Num.max() is {numberArr.Reverse()}");//
-            Console.WriteLine($"The maximum number using Num.max() is {numberArr.Sum()}");//
-            Console.WriteLine($"The maximum number using Num.max() is {numberArr.Average()}");//
+            int[] numberArr = new int[] { 9, 6, 5, 1, 2, 7, 4, 8, 3, 0 };
+            Console.WriteLine($"The maximum number using Num.Max() is {numberArr.Max()}");//
+            Console.WriteLine($"The Minimum number using Num.Min() is {numberArr.Min()}");//
+            //Console.WriteLine($"The maximum number using Num.max() is {numberArr.Reverse()}");//
+            Console.WriteLine($"The maximum number using Num.Sum() is {numberArr.Sum()}");//
+            Console.WriteLine($"The maximum number using Num.Average() is {numberArr.Average()}");//
 
             Console.WriteLine("##################");//Spacing Each Solution
+            Console.WriteLine("Sorting an Array requires outputting an array");//Spacing Each Solution
+            Array.Sort(numberArr);
+            foreach (var sorted in numberArr)
+                Console.WriteLine(sorted);
+
+            Console.WriteLine("Reversing an Array requires outputting an array");//Spacing Each Solution
+            Array.Reverse(numberArr);
+            foreach (var reversed in numberArr)
+            {
+                Console.Write(reversed);
+            }
+            Console.WriteLine("");
+            //string str = "Hello";
+            //char[] chars = str.ToCharArray();
+            char[] chars = { 't', 'i', 'm', 'e' };
+            Array.Reverse(chars);
+            foreach (char chara in chars)
+            {
+                Console.Write(chara);
+            }
+            Console.WriteLine("");
+            IQueryable<char> answ = chars.AsQueryable().Reverse();
+            foreach (char str in answ)
+            {
+                Console.Write(str);
+            }
+            string herm = "Hello";
+            string[] grease = herm.Split();
+            foreach (string chara in grease) {
+                Console.WriteLine(chara);
+            }
+
+
+            //IQueryable<char> res = chars.AsQueryable().Reverse();
+            //Console.Write("\nReversed String = ");
+            //foreach (char chara in res)
+            //{
+            //    Console.Write(chara);
+            //}
+            //Console.WriteLine($"The maximum number using Num.max() is {Array.Reverse(numberArr)}");//
+            //Console.WriteLine($"The maximum number using Num.max() is {Array.Reverse()}");//
+            //Console.WriteLine($"The maximum number using Num.max() is {numberArr.Sum()}");//
+            //Console.WriteLine($"The maximum number using Num.max() is {numberArr.Average()}");//
 
             Console.WriteLine("");
         }
